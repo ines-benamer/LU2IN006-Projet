a28be6aba4ff1f2abbbc94fd31cc1f12587231af
ines-benamer <ines.benamerbelkacem@gmail.com>
2024-02-29 13:02:39 +0100

    ajout du main (ne compile pas pour l'instant car il faut définir la fonction de lecture d'un fichier)

---
+++ Code/ChaineMain.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "Chaine.h"
+#include "SVGwriter.h"
+
+int main(int argc, char* argv) {
+
+    if (argc!=2) {
+        printf("Usage : <nom du fichier à lire>");
+    }
+    FILE* f = fopen(argv[1], "r");
+
+    Chaines* fic_lu = lectureChaines(f);
+
+    FILE* f2 = fopen("test_ecriture.txt", "r");
+    ecrireChaines(f2);
+
+    return 0;
+}
+
+
