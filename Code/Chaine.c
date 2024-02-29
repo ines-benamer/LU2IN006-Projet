0a4a27cd3d952227514333ffe087641521e33cc0
ines-benamer <ines.benamerbelkacem@gmail.com>
2024-02-29 12:59:43 +0100

    ajout de la fonction exritureChaines

--- Code/Chaine.c
+++ Code/Chaine.c
@@ -2,4 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+
 #include "Chaine.h"
+#include "SVGwriter.h"
+
+
+// Question 2
+
+void ecrireChaines(Chaines* C, FILE* f) {
+
+    fprintf(f, "NbChain: %d\n", C->nbChaines);
+    fprintf(f, "Gamma: %d\n", C->gamma);
+    CellChaine* chaine_cour = C->chaines;
+
+    char* tmp = "";
+
+    while (chaine_cour) {
+        fprintf(f, "%d ", chaine_cour->numero);
+        int compteur = 0;
+        char* str = ""; // Chaîne de caractères temporaire dans laquelle on va stocker les points de la ligne
+        CellPoint* point_cour = chaine_cour->points;
+
+        while (point_cour) { // On parcourt la liste chaînée de points en 
+            sprintf(tmp, "%f", point_cour->x);
+            strcat(str, tmp);
+            strcat(str, " ");
+            sprintf(tmp, "%f", point_cour->y);
+            strcat(str, tmp);
+            strcat(str, " ");
+            compteur++;
+            point_cour = point_cour->suiv;
+        }
+        fprintf(f, "%d %s\n", compteur, str);
+        chaine_cour = chaine_cour->suiv;
+    }
+}
