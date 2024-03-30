fb05969281c431d9ef1538c7770dc88e68a4df95
louisabouali <louizabouali2@gmail.com>
2024-03-30 18:45:06 +0100

    ajout du fichier hachage.h

--- Code/Hachage.h
+++ Code/Hachage.h
@@ -1,5 +1,13 @@
-typedef struct{
-  int nbElement; //pas necessaire ici
-  int tailleMax;
-  CellNoeud** T;
-} TableHachage ;
+#ifndef __HACHAGE_H__
+#define __HACHAGE_H__
+#include <stdio.h>
+#include "Reseau.h"
+
+typedef struct table {
+    cellnoeud** table;
+    int lenght;
+} TableHachage;
+
+
+
+#endif
